37584bf99d049a8f2af3cabf9a2a4d32949a5774
AlirezaMahmoodi <alireza.mahmoodi.amai@gmail.com>
2022-11-07 22:20:03 +0330

    Selection & basic ROLLING logic

---
+++ js.js
@@ -0,0 +1,25 @@
+const score0 = document.getElementById("score--0");
+const score1 = document.getElementById("score--1");
+const current0 = document.getElementById("current--0");
+const current1 = document.getElementById("current--1");
+const diceEl = document.querySelector(".dice");
+const newBtn = document.querySelector(".btn--new");
+const rollBtn = document.querySelector(".btn--roll");
+const holdBtn = document.querySelector(".btn--hold");
+
+// start
+diceEl.classList.add("hidden");
+
+// Roll logic
+rollBtn.addEventListener("click", genNum);
+
+let currentScore = 0;
+function genNum() {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    current0.textContent = currentScore;
+  }
+}
