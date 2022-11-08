0bb90ae3ef6e64bcc45b6840a3ce61839beec68e
AlirezaMahmoodi <alireza.mahmoodi.amai@gmail.com>
2022-11-08 22:30:07 +0330

    switch logic background

--- js.js
+++ js.js
@@ -6,6 +6,8 @@ const diceEl = document.querySelector(".dice");
 const newBtn = document.querySelector(".btn--new");
 const rollBtn = document.querySelector(".btn--roll");
 const holdBtn = document.querySelector(".btn--hold");
+const player0 = document.querySelector(".player--0");
+const player1 = document.querySelector(".player--1");
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -30,5 +32,7 @@ function genNum() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player0.classList.toggle("player--active");
+    player1.classList.toggle("player--active");
   }
 }
